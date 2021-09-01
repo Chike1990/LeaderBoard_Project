a04b829aa2b64b7c53f4d28d6d35bd78bb87f4a0
Chike1990 <chibuikeuzoechina@gmail.com>
2021-09-01 13:04:52 +0100

    Set up the page structure and style

--- src/index.js
+++ src/index.js
@@ -1,6 +1,41 @@
 import _ from 'lodash';
 import './style.css';
 
+
+let score = [
+  {
+    Name: 'Chike',
+    score: 71
+  },
+  {
+    Name: 'Nelsino',
+    score: 83
+  },
+  {
+    Name: 'Jose',
+    score: 78
+  },
+  {
+    Name: 'Dante',
+    score: 80
+  },
+  {
+    Name: 'Hamza',
+    score: 97
+  },
+  {
+    Name: 'Agbo',
+    score: 76
+  },
+  {
+    Name: 'Enio',
+    score: 98
+  },
+]
+
+
+
+
 function component() {
   const element = document.createElement('div');
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,9 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
+  mode: 'none',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
@@ -13,5 +15,10 @@ module.exports = {
          use: ['style-loader', 'css-loader'],
         },
       ],
-    },
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
 };
