a56125a977e71b792a2d34f11ce6fe6505d4691b
Chike1990 <chibuikeuzoechina@gmail.com>
2021-09-01 21:18:28 +0100

    Create scoredata.js for storing score data

---
+++ src/scoredata.js
@@ -0,0 +1,32 @@
+
+let score = [
+  {
+    Name: 'Chike',
+    score: 71
+  },
+  {
+    Name: 'Nelsino',
+    score: 83
+  },
+  {
+    Name: 'Jose',
+    score: 78
+  },
+  {
+    Name: 'Dante',
+    score: 80
+  },
+  {
+    Name: 'Hamza',
+    score: 97
+  },
+  {
+    Name: 'Agbo',
+    score: 76
+  },
+  {
+    Name: 'Enio',
+    score: 98
+  },
+]
+
